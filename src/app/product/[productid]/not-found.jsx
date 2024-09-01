cedd9e04661d1ae8175d619500a39c8571d0f2f3
devsultan06 <sultanabaniks@gamil.com>
2024-09-01 13:32:30 +0100

    learnt about Not Found Page in Next

---
+++ src/app/product/[productid]/not-found.jsx
@@ -0,0 +1,3 @@
+export default function ProductDetails() {
+  return <h1>Review Page Not found</h1>;
+}
--- src/app/product/[productid]/page.tsx
+++ src/app/product/[productid]/page.tsx
@@ -1,7 +1,12 @@
+import { notFound } from "next/navigation";
 export default function ProductDetails({
   params,
 }: {
   params: { productid: string };
 }) {
+  if (parseInt(params.productid) > 1000) {
+    notFound();
+  }
+
   return <h1>Product Details Page {params.productid}</h1>;
 }
