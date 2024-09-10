1fc10a1bcddb442d9f5569fa16bd1cf2ef7b21b5
Sultan A. Dev <sultanabaniks@gmail.com>
2024-09-10 10:52:56 +0100

    error

--- src/app/product/[productid]/reviews/[reviewsId]/page.tsx
+++ src/app/product/[productid]/reviews/[reviewsId]/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewsDetails({
   params,
 }: {
   params: { productid: string; reviewsId: string };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Review not found");
+  }
+
   return (
     <h1>
       Review {params.reviewsId} for Product {params.productid}
